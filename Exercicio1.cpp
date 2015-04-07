b596d99ee7abc24b05b702ac9cd143928ff1b715
Walter Rebolo <w.rebolo01@gmail.com>
2015-04-07 12:06:51 -0300

    Exercicio 01

--- Exercicio1.cpp
+++ Exercicio1.cpp
@@ -14,58 +14,31 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	int vet1[SIZE];
 	int vet2[SIZE];
-	
+	int j;
 
 	srand(time(NULL));
-	for (int i = 0; i < SIZE; i++)
+
+	for ( j = 0; j < SIZE; j++)
 	{
-		vet1[i] = rand() % 100;
-		vet2[i] = rand() % 100;
-		printf("Vetor 1: %d\nVetor 2: %d\n", vet1[i], vet2[i]);
-		printf("Soma: %d\n", SomaVetores(vet1, vet2, SIZE));
+		vet1[j] = rand() % 100;
+	    vet2[j] = rand() % 100;
+		printf("Vetor 1: %d\nVetor 2: %d\n", vet1[j], vet2[j]);
+		printf("Soma: %d\n\n", SomaVetores(vet1, vet2, SIZE));
 	}
-	
-
-/*	for (int i = 0; i < SIZE; i++)
-	{
-		vet1[i] = rand() % 100;
-		vet2[i] = rand() % 100;
-		
-		printf("Vetor1: %d\nVetor2: %d\n", vet1[i], vet2[i]);
-
-		soma[i] = vet1[i] + vet2[i];
-		printf("Soma: %d\n\n", soma[i]);*/
-	//}
-
 	return 0;
 }
 
 int SomaVetores(int n[], int m[], int tamanho)
 {
 	int soma[SIZE] = { 0, 0, 0 };
-	/*int vet1[SIZE];
-	int vet2[SIZE];
-	int soma[SIZE];
-
-	for (int i = 0; i < SIZE; i++)
-	{
-		vet1[i] = rand() % 100;
-		vet2[i] = rand() % 100;*/
-
-//		printf("Vetor1: %d\nVetor2: %d\n", vet1[i], vet2[i]);
-
-		//soma[i] = vet1[i] + vet2[i];
-//		printf("Soma: %d\n\n", soma[i]);
-	int i;
 	int aux[SIZE] = { 0, 0, 0 };
+	int i;
+
 	for (i = 0; i < SIZE;)
 	{
 		aux[i] = n[i] + m[i];
+		soma[i] += aux[i];
 		
-		
-		return soma[i] = aux[i];
+		return soma[i];
 	}
-	
-	
-	
 }
