6632516a9852d6da58c83ca994a002596ecf60f6
Walter Rebolo <w.rebolo01@gmail.com>
2015-04-07 15:44:25 -0300

    Exercicio 01

--- Exercicio1.cpp
+++ Exercicio1.cpp
@@ -8,37 +8,38 @@
 
 #define SIZE 3
 
-int SomaVetores(int n[], int m[], int tamanho);
+int SomaVetores(int n, int m, int tamanho);
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	int vet1[SIZE];
 	int vet2[SIZE];
-	int j;
 
 	srand(time(NULL));
 
+	int j;
 	for ( j = 0; j < SIZE; j++)
 	{
 		vet1[j] = rand() % 100;
 	    vet2[j] = rand() % 100;
 		printf("Vetor 1: %d\nVetor 2: %d\n", vet1[j], vet2[j]);
-		printf("Soma: %d\n\n", SomaVetores(vet1, vet2, SIZE));
-	}
+		printf("Soma: %d\n\n", SomaVetores (vet1[j], vet2[j], SIZE) );
+	
+		system("pause");
+		} 
 	return 0;
 }
 
-int SomaVetores(int n[], int m[], int tamanho)
+int SomaVetores(int n, int m, int tamanho)
 {
-	int soma[SIZE] = { 0, 0, 0 };
-	int aux[SIZE] = { 0, 0, 0 };
+	int SomaTotal(int x, int y, int tamanho);
 	int i;
+	int auxSoma = 0;
+	int soma = 0;
 
-	for (i = 0; i < SIZE;)
+	for (i = 0; i < SIZE; i++)
 	{
-		aux[i] = n[i] + m[i];
-		soma[i] += aux[i];
-		
-		return soma[i];
+		auxSoma = n + m;
+		return soma += auxSoma;
 	}
 }
